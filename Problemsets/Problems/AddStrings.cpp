692ff39c74d01402060be4bf54c36e3b8d0a7a1d
Bobby <s215818@dtu.dk>
2024-04-16 15:23:16 +0200

    AddStrings.cpp

---
+++ Problemsets/Problems/AddStrings.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        int n = num1.size(), m = num2.size();
+        std::string ans = "";
+        int carry = 0;
+
+        int idx = std::max(n, m);
+        for (int i = 0; i < idx; ++i) {
+            int a = i < n ? num1[n - i - 1] - '0' : 0;
+            int b = i < m ? num2[m - i - 1] - '0' : 0;
+
+            int sum = a + b + carry;
+            carry = sum / 10;
+            sum %= 10;
+
+            ans += std::to_string(sum);
+        }
+
+        if (carry) {
+            ans += std::to_string(carry);
+        }
+        std::reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
