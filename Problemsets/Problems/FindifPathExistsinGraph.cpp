7ea5a5ede304660151752d4d7f27320ac73699bb
Bobby <s215818@dtu.dk>
2024-04-22 15:38:12 +0200

    FindifPathExistsinGraph.cpp

---
+++ Problemsets/Problems/FindifPathExistsinGraph.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+
+
+class Solution {
+public:
+    bool dfs(int source, int destination, std::vector<int> adj[], std::vector<int> &visited) {
+        visited[source] = 1;
+        for (auto it : adj[source]) {
+            if (!visited[it]) {
+                if (it == destination) return true;
+                if (dfs(it, destination, adj, visited)) return true;
+            }
+        }
+        return false;
+    }
+
+    bool validPath(int n, std::vector<std::vector<int>>& edges, int source, int destination) {
+        std::vector<int> adj[n];
+        for (auto it : edges) {
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
+        }
+        if (source == destination) return true;
+        std::vector<int> visited(n,0);
+        return dfs(source, destination, adj, visited);
+    }
+};
