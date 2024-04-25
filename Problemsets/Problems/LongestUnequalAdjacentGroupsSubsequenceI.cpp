f53b1839ec5c225be903e03535cb5f5f755616db
Bobby <s215818@dtu.dk>
2024-04-25 06:08:29 +0200

    LongestUnequalAdjacentGroupsSubsequenceI.cpp

--- Problemsets/Problems/LongestUnequalAdjacentGroupsSubsequenceI.cpp
+++ Problemsets/Problems/LongestUnequalAdjacentGroupsSubsequenceI.cpp
@@ -1,6 +1,18 @@
+#include <string>
+#include <vector>
+
 class Solution {
 public:
-    vector<string> getLongestSubsequence(vector<string>& words, vector<int>& groups) {
-
+    std::vector<std::string> getLongestSubsequence(std::vector<std::string>& words, std::vector<int>& groups) {
+        std::vector<std::string> ans;
+        ans.push_back(words[0]);
+        int prev = groups[0];
+        for (int i = 1; i < words.size(); ++i) {
+            if (groups[i] != prev) {
+                ans.push_back(words[i]);
+                prev=groups[i];
+            }
+        }
+        return ans;
     }
 };
