779d71e9b063ca3eb8d0fc84ea4ffffc33abf83b
Deniz <s215818@dtu.dk>
2024-05-18 00:24:23 +0200

    CoinChangeII.cpp

--- Problemsets/Problems/CoinChangeII.cpp
+++ Problemsets/Problems/CoinChangeII.cpp
@@ -1,11 +1,14 @@
+#include <vector>
+
 class Solution {
 public:
     int change(int amount, std::vector<int>& coins) {
         std::vector<int> path(amount + 1, 0);
         path[0] = 1;
-        for (int i = 0; i < coins.size(); i++) {
-            for (int j = coins[i]; j <= amount; j++) {
-                path[j] += path[j - coins[i]];
+
+        for (int coin : coins) {
+            for (int i = coin; i < amount+1; ++i) {
+                path[i] += path[i - coin];
             }
         }
         return path[amount];
