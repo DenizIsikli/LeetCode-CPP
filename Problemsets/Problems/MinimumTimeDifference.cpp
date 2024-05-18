6144f16ee5bfd0f4b2fd5ab4534ea5651e40b865
Deniz <s215818@dtu.dk>
2024-05-18 23:17:21 +0200

    MinimumTimeDifference.cpp

---
+++ Problemsets/Problems/MinimumTimeDifference.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+
+class Solution {
+public:
+    int findMinDifference(std::vector<std::string>& timePoints) {
+        std::vector<int> minutes;
+        for (auto time : timePoints) {
+            int hour = std::stoi(time.substr(0, 2));
+            int minute = std::stoi(time.substr(3, 2));
+            minutes.push_back(hour * 60 + minute);
+        }
+        std::sort(minutes.begin(), minutes.end());
+        int minDiff = INT_MAX;
+        for (int i = 0; i < minutes.size(); i++) {
+            for (int j = i+1; j < minutes.size(); j++) {
+                minDiff = std::min(minDiff, std::min(minutes[j] - minutes[i], 1440 - minutes[j] + minutes[i]));
+            }
+        }
+        return minDiff;
+    }
+};
