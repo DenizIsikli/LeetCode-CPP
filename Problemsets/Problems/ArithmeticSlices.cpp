b856f2e53f416b7b737812539b97654a228e5d15
Deniz <s215818@dtu.dk>
2024-05-21 22:22:54 +0200

    ArithmeticSlices.cpp

--- Problemsets/Problems/ArithmeticSlices.cpp
+++ Problemsets/Problems/ArithmeticSlices.cpp
@@ -4,12 +4,12 @@ class Solution {
 public:
     int numberOfArithmeticSlices(std::vector<int>& nums) {
         if(nums.size()<3) return 0;
-        std::vector<int> dp(n, 0);
+        std::vector<int> dp(nums.size(), 0);
         int ans = 0;
         for (int i = 2; i < nums.size(); ++i) {
             if (nums[i]-nums[i-1] == nums[i-1]-nums[i-2]) {
                 dp[i] = dp[i-1]+1;
-                ans 
+                ans+=dp[i];
             }
         }
         return ans;
