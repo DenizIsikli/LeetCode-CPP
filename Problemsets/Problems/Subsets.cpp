5855133492632d0bf32ab26d15f839c04031f5e0
Deniz <s215818@dtu.dk>
2024-05-21 22:40:58 +0200

    Subsets.cpp

---
+++ Problemsets/Problems/Subsets.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+
+class Solution {
+public:
+    std::vector<std::vector<int>> subsets(std::vector<int>& nums) {
+        std::vector<std::vector<int>> ans;
+        std::vector<int> path;
+        backtrack(nums,0,path,ans);
+        return ans;
+    }
+
+    void backtrack(std::vector<int> &nums, int start, std::vector<int> &path, std::vector<std::vector<int>> &ans) {
+        ans.push_back(path);
+        for (int i = start; i < nums.size(); ++i) {
+            path.push_back(nums[i]);
+            backtrack(nums, i+1, path, ans);
+            path.pop_back();
+        }
+    }
+};
+
