46964a538d1a462a43a3b25ebe7c7a09e86c0858
Deniz <s215818@dtu.dk>
2024-05-24 01:09:09 +0200

    TheNumberofBeautifulSubsets.cpp

---
+++ Problemsets/Problems/TheNumberofBeautifulSubsets.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int count;
+    std::unordered_map<int, int> memo;
+
+    void backtracking(std::vector<int> &nums, int k, int idx) {
+        if (idx == nums.size()) {
+            count++;
+            return;
+        }
+
+        int num = nums[idx];
+        if (memo.find(num-k) == memo.end() && memo.find(num+k) == memo.end()) {
+            memo[num]++;
+            backtracking(nums, k, idx+1);
+            memo[num]--;
+            if (memo[num] == 0) {
+                memo.erase(num);
+            }
+        }
+        backtracking(nums, k, idx+1);
+    }
+    
+    int beautifulSubsets(vector<int>& nums, int k) {
+        count = 0;
+        backtracking(nums, k, 0);
+        return count-1;
+    }
+};
+
