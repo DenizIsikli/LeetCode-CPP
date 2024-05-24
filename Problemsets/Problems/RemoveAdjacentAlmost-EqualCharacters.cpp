2f6e61e4c00778df32c1a1c4e83d1584d8ee8f77
Deniz <s215818@dtu.dk>
2024-05-24 18:56:43 +0200

    RemoveAdjacentAlmost-EqualCharacters.cpp

---
+++ Problemsets/Problems/RemoveAdjacentAlmost-EqualCharacters.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int removeAlmostEqualCharacters(string word) {
+        int ans = 0;
+        for (int i = 1; i < word.size(); ++i) {
+            if (word[i] >= word[i-1] && word[i]-word[i-1] <= 1) ans++, i++;
+            else if (word[i] < word[i-1] && word[i-1]-word[i] <= 1) ans++, i++;
+        }
+        return ans;
+    }
+};
