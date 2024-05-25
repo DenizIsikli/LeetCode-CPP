e69ed4233dc6b504284b2e37947307d9629fd598
Deniz <s215818@dtu.dk>
2024-05-25 22:08:54 +0200

    Non-decreasingSubsequences.cpp

---
+++ Problemsets/Problems/Non-decreasingSubsequences.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void backtracking(std::vector<int> &nums, int start, std::vector<int> &path, std::vector<std::vector<int>> &ans) {
+        if (path.size()>=2) ans.push_back(path);
+        std::unordered_set<int> used;
+        for (int i = start; i < nums.size(); ++i) {
+            if (!path.empty() && nums[i]<path.back()) continue;
+            if (used.count(nums[i])) continue;
+            used.insert(nums[i]);
+            path.push_back(nums[i]);
+            backtracking(nums, i+1, path, ans);
+            path.pop_back();
+        }
+    }
+
+    std::vector<std::vector<int>> findSubsequences(std::vector<int>& nums) {
+        std::vector<std::vector<int>> ans;
+        std::vector<int> path;
+        backtracking(nums, 0, path, ans);
+        return ans;
+    }
+};
