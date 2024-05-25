2ac9e8b3666e5383abd2f8852b1cc9a05cd0db66
Deniz <s215818@dtu.dk>
2024-05-26 01:51:36 +0200

    BeautifulArrangement.cpp

---
+++ Problemsets/Problems/BeautifulArrangement.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void backtrack(std::vector<int> nums, int start, int &ans) {
+        if (start==nums.size()) {
+            ans++;
+            return;
+        }
+
+        for (int i = start; i < nums.size(); ++i) {
+            std::swap(nums[start], nums[i]);
+            if (nums[start]%(start+1)==0 || (start+1)%nums[start]==0)
+                backtrack(nums, start+1, ans);
+            std::swap(nums[start], nums[i]);
+        }
+    }
+
+    int countArrangement(int n) {
+        std::vector<int> nums;
+        for (int i = 1; i <= n; ++i) nums.push_back(i);
+        int ans;
+        backtrack(nums, 0, ans);
+        return ans;
+    }
+};
