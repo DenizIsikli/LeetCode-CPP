88c4372157df0c74e94f8e2968cf5a784697831c
Deniz <s215818@dtu.dk>
2024-06-14 22:56:19 +0200

    MinimumIncrementtoMakeArrayUnique.cpp

---
+++ Problemsets/Problems/MinimumIncrementtoMakeArrayUnique.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    int minIncrementForUnique(std::vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+        int moves = 0;
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i] <= nums[i - 1]) {
+                moves += nums[i - 1] - nums[i] + 1;
+                nums[i] = nums[i - 1] + 1;
+            }
+        }
+        return moves;
+    }
+};
