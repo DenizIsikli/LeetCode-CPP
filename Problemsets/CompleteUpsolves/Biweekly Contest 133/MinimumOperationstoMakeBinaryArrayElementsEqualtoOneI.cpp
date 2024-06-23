7880989ecbc1177174b18c85085b177dd31c4463
Deniz <s215818@dtu.dk>
2024-06-23 14:32:37 +0200

    MinimumOperationstoMakeBinaryArrayElementsEqualtoOneI.cpp

---
+++ Problemsets/CompleteUpsolves/Biweekly Contest 133/MinimumOperationstoMakeBinaryArrayElementsEqualtoOneI.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+
+class Solution {
+public:
+    int minOperations(std::vector<int>& nums) {
+        int ans = 0;
+        for (int i = 0; i < nums.size()-2; ++i) {
+            if (nums[i]==0) {
+                nums[i] = 1;
+                nums[i+1] = 1-nums[i+1];
+                nums[i+2] = 1-nums[i+2];
+                ++ans;
+            }
+        }
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i]==0) return -1;
+        }
+        return ans;
+    }
+};
