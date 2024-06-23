d01c25669af33703b000d3a1609c3e75d92b43c5
Deniz <s215818@dtu.dk>
2024-06-23 15:15:43 +0200

    FindtheMinimumAreatoCoverAllOnesI.cpp

---
+++ Problemsets/CompleteUpsolves/Weekly Contest 403/FindtheMinimumAreatoCoverAllOnesI.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+
+class Solution {
+public:
+    int minimumArea(std::vector<std::vector<int>>& grid) {
+        int minrow = INT_MAX, maxrow = INT_MIN, mincol = INT_MAX, maxcol = INT_MIN;
+        for (int i = 0; i < grid.size(); ++i) {
+            for (int j = 0; j < grid[0].size(); ++j) {
+                if (grid[i][j]==1) {
+                    minrow = std::min(minrow, i);
+                    maxrow = std::max(maxrow, i);
+                    mincol = std::min(mincol, j);
+                    maxcol = std::max(maxcol, j);
+                }
+            }
+        }
+        return (maxrow-minrow+1)*(maxcol-mincol+1);
+    }
+};
