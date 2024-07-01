2335b299d8e3ae611d0382de9f60ec2d072ed5f5
Deniz <s215818@dtu.dk>
2024-07-01 23:28:43 +0200

    ThreeConsecutiveOdds.cpp

---
+++ Problemsets/Problems/ThreeConsecutiveOdds.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+
+class Solution {
+public:
+    bool threeConsecutiveOdds(std::vector<int>& arr) {
+        int count = 0;
+
+        for (int num : arr) {
+            if (num%2==1) {
+                ++count;
+                if (count == 3) return true;
+            } else count = 0;
+        }
+        return false;
+    }
+};
