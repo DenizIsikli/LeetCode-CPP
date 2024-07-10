061e64aa79b204132619317086345d44f5e76f34
Deniz <s215818@dtu.dk>
2024-07-10 02:24:01 +0200

    CrawlerLogFolder.cpp

---
+++ Problemsets/Problems/CrawlerLogFolder.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+class Solution {
+public:
+    int minOperations(std::vector<std::string>& logs) {
+        int ans = 0;
+        for (const auto &log : logs) {
+            if (log == "../") {
+                if (ans > 0) {
+                    --ans;
+                }
+            } else if (log != "./") {
+                ++ans;
+            }
+        }
+        return ans;
+    }
+};
