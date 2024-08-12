2ebfee1910f1754594698e4617dec07ca0739a2d
Deniz Isikli <s215818@dtu.dk>
2024-08-12 21:19:50 +0200

    KthLargestElementinaStream.cpp

---
+++ Problemsets/Problems/KthLargestElementinaStream.cpp
@@ -0,0 +1,27 @@
+#include <queue>
+#include <vector>
+
+class KthLargest {
+public:
+    int k;
+    std::priority_queue<int, std::vector<int>, std::greater<int>> pq;
+    KthLargest(int k, std::vector<int>& nums) {
+        this->k = k;
+        for (int &num : nums) {
+            pq.push(num);
+            if (pq.size() > k) pq.pop();
+        }
+    }
+    
+    int add(int val) {
+        pq.push(val);
+        if (pq.size() > k) pq.pop();
+        return pq.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
