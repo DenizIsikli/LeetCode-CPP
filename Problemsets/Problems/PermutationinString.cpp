ecd9d546bdac130cef0a163f961133be9f0586c7
Deniz Isikli <s215818@dtu.dk>
2024-10-05 22:17:45 +0200

    PermutationinString.cpp

--- Problemsets/Problems/PermutationinString.cpp
+++ Problemsets/Problems/PermutationinString.cpp
@@ -4,16 +4,17 @@
 class Solution {
 public:
     bool checkInclusion(std::string s1, std::string s2) {
+        if (s1.size() > s2.size()) {
+            return false;
+        }
         std::sort(s1.begin(), s1.end());
-        bool res = false;
-        for (int i = 0; i < s2.size()-s1.size()+1; i++) {
-            std::string temp = s2.substr(i, s1.size());
-            std::sort(temp.begin(), temp.end());
-            if (temp == s1) { 
-                res = true; 
-                break;
+        for (int i = 0; i <= s2.size() - s1.size(); i++) {
+            std::string sub = s2.substr(i, s1.size());
+            std::sort(sub.begin(), sub.end());
+            if (sub == s1) {
+                return true;
             }
         }
-        return res;
+        return false;
     }
 };
