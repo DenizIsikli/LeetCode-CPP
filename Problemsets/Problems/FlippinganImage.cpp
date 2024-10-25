e0929ab6a7f41fc0c642527f2900e12fb4df5ae9
Deniz Isikli <s215818@dtu.dk>
2024-10-25 23:39:29 +0200

    FlippinganImage.cpp

---
+++ Problemsets/Problems/FlippinganImage.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+
+class Solution {
+public:
+    std::vector<std::vector<int>> flipAndInvertImage(std::vector<std::vector<int>>& image) {
+        for (int i = 0; i < image.size(); i++) {
+            std::reverse(image[i].begin(), image[i].end());
+            for (int j = 0; j < image[i].size(); j++) {
+                image[i][j] = image[i][j] == 0 ? 1 : 0;
+            }
+        }
+        return image;
+    }
+};
