4dc84fdbd87259d8244cd7ab1c9348a820f58594
Deniz Isikli <s215818@dtu.dk>
2024-11-01 22:48:41 +0100

    DeleteCharacterstoMakeFancyString.cpp

---
+++ Problemsets/Problems/DeleteCharacterstoMakeFancyString.cpp
@@ -0,0 +1,20 @@
+#include <string>
+
+class Solution {
+public:
+    std::string makeFancyString(std::string s) {
+        std::string res = "";
+        res.push_back(s[0]);
+        int cnt = 1;
+        for (int i = 1; i < s.size(); i++) {
+            if (s[i]==res.back()) {
+                cnt++;
+                if (cnt<3) res.push_back(s[i]);
+            } else {
+                cnt = 1;
+                res.push_back(s[i]);
+            }
+        }
+        return res;
+    }
+};
