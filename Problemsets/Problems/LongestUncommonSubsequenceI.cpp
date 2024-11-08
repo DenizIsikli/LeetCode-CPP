15666ffd383136dffbbe47b9a78f9e90eb2f75a5
Deniz Isikli <s215818@dtu.dk>
2024-11-08 22:19:10 +0100

    LongestUncommonSubsequenceI.cpp

---
+++ Problemsets/Problems/LongestUncommonSubsequenceI.cpp
@@ -0,0 +1,9 @@
+#include <string>
+
+class Solution {
+public:
+    int findLUSlength(std::string a, std::string b) {
+        if (a == b) return -1;
+        return std::max(a.size(), b.size());
+    }
+};
