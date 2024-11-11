6be7f5abc0e03a3c4547a198a41e2e373a099ed5
Deniz Isikli <s215818@dtu.dk>
2024-11-11 21:53:01 +0100

    PrimeSubtractionOperation.cpp

---
+++ Problemsets/Problems/PrimeSubtractionOperation.cpp
@@ -0,0 +1,29 @@
+#include <math.h>
+#include <vector>
+
+class Solution {
+public:
+    bool primeSubOperation(std::vector<int>& nums) {
+        auto checkPrime = [](int x) -> bool {
+            if (x<=1) return false;
+            for (int i = 2; i <= sqrt(x); i++) {
+                if (x%i==0) return false;
+            }
+            return true;
+        };
+
+        for (int i = 0; i < nums.size(); i++) {
+            int bound = (i==0) ? nums[i] : nums[i]-nums[i-1];
+            if (bound<=0) return false;
+            int largestPrime = 0;
+            for (int j = bound-1; j >= 2; j--) {
+                if (checkPrime(j)) {
+                    largestPrime = j;
+                    break;
+                }
+            }
+            nums[i]-=largestPrime;
+        }
+        return true;
+    }
+};
