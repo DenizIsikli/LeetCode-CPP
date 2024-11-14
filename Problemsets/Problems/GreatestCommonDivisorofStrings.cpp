c9f008d342bdf161a0e9207bf1e7d3c8bf5cd869
Deniz Isikli <s215818@dtu.dk>
2024-11-14 22:24:18 +0100

    GreatestCommonDivisorofStrings.cpp

---
+++ Problemsets/Problems/GreatestCommonDivisorofStrings.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <numeric>
+
+class Solution {
+public:
+    std::string gcdOfStrings(std::string str1, std::string str2) {
+        if (str1 + str2 != str2 + str1) return "";
+        return str1.substr(0, std::gcd(str1.size(), str2.size()));
+    }
+};
