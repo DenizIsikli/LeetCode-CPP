1d00a9cf97d73afa92dbcffbdae3903b197a7098
Deniz Isikli <s215818@dtu.dk>
2024-11-15 23:45:26 +0100

    ShortestSubarraytobeRemovedtoMakeArraySorted.cpp

---
+++ Problemsets/Problems/ShortestSubarraytobeRemovedtoMakeArraySorted.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+
+class Solution {
+public:
+    int findLengthOfShortestSubarray(std::vector<int>& arr) {
+        int n = arr.size(), j=n-1;
+        while(j > 0 && arr[j] >= arr[j-1]){
+            j--;
+        }
+
+        int ans = j, i = 0;
+        while(i < j && (i == 0 || arr[i-1] <= arr[i])){
+            while(j < n && arr[i] > arr[j]){
+                j++;
+            }
+            ans = std::min(ans, j-i-1);
+            i++;
+        }
+        return ans;
+    }
+};
