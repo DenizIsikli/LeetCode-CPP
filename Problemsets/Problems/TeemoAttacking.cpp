ccdaffa3ffdec7a3040dccce9ec832e6a9718b5b
Deniz <s215818@dtu.dk>
2024-12-30 19:23:05 +0100

    Misc

--- Problemsets/Problems/TeemoAttacking.cpp
+++ Problemsets/Problems/TeemoAttacking.cpp
@@ -6,7 +6,7 @@ public:
         int res = 0;
         for (int i = 0; i < timeSeries.size(); i++) {
             if (i<timeSeries.size()-1 && timeSeries[i]+duration>timeSeries[i+1]) {
-                res += timeSeries[i+1]-timeSeries[i]; 
+                res += timeSeries[i+1]-timeSeries[i];
             } else {
                 res += duration;
             }
