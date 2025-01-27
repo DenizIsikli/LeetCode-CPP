47e93c9a9bd57b13956206ade6b15f6e90a627ce
Deniz <s215818@dtu.dk>
2025-01-27 23:53:39 +0100

    CourseScheduleIV.cpp

---
+++ Problemsets/Problems/CourseScheduleIV.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
+        vector<vector<bool>> graph(numCourses, vector<bool>(numCourses, false));
+        for (auto &p : prerequisites) {
+            graph[p[0]][p[1]] = true;
+        }
+
+        for (int k = 0; k < numCourses; ++k) {
+            for (int i = 0; i < numCourses; ++i) {
+                for (int j = 0; j < numCourses; ++j) {
+                    graph[i][j] = graph[i][j] || (graph[i][k] && graph[k][j]);
+                }
+            }
+        }
+
+        vector<bool> res;
+        for (auto &q : queries) {
+            res.push_back(graph[q[0]][q[1]]);
+        }
+
+        return res;
+    }
+};
