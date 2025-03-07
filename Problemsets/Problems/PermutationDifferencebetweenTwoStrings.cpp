454cbb818a2d3df80afea2a57caaccdd8e444cfc
Deniz <s215818@dtu.dk>
2025-03-07 23:04:32 +0100

    PermutationDifferencebetweenTwoStrings.cpp

---
+++ Problemsets/Problems/PermutationDifferencebetweenTwoStrings.cpp
@@ -0,0 +1,16 @@
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    int findPermutationDifference(string s, string t) {
+        int res = 0;
+        for (int i = 0; i < s.size(); i++) {
+            for (int j = 0; j < s.size(); j++) {
+                if (s[i]==t[j]) res+=abs(i-j);
+            }
+        }
+        return res;
+    }
+};
