b26c7b96ca6e6b8f401749dc9ea05341b28fd1e2
Deniz <s215818@dtu.dk>
2025-03-13 00:09:37 +0100

    DecodeXORedArray.cpp

---
+++ Problemsets/Problems/DecodeXORedArray.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> decode(vector<int>& encoded, int first) {
+        vector<int> decoded;       
+        decoded.push_back(first);
+        for (int i = 0; i < encoded.size(); i++) {
+            decoded.push_back(encoded[i] ^ decoded[i]); 
+        }
+        return decoded;
+    }
+};
