1138b8f605f099a64c8066019d148155e738da12
Deniz <s215818@dtu.dk>
2025-03-12 23:38:14 +0100

    MaximumCountofPositiveIntegerandNegativeInteger.cpp

---
+++ Problemsets/Problems/MaximumCountofPositiveIntegerandNegativeInteger.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maximumCount(vector<int>& nums) {
+        int pos=0, neg=0;
+        for (auto &x : nums) {
+            if (x>0) pos++;
+            else if (x<0) neg++;
+        }
+        return max(pos,neg);
+    }
+};
+
