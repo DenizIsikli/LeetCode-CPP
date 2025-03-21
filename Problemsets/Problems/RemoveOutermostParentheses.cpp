a12f9276f0e6b07874080fe605eb369165be4d10
Deniz <s215818@dtu.dk>
2025-03-21 21:18:43 +0100

    RemoveOutermostParentheses.cpp

---
+++ Problemsets/Problems/RemoveOutermostParentheses.cpp
@@ -0,0 +1,26 @@
+#include <stack>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        stack<char> st;
+        string res;
+        for (char c : s) {
+            if (c=='(') {
+                if (!st.empty()) {
+                    res+=c;
+                }
+                st.push(c);
+            } else {
+                st.pop();
+                if (!st.empty()) {
+                    res+=c;
+                }
+            }
+        }
+        return res;
+    }
+};
