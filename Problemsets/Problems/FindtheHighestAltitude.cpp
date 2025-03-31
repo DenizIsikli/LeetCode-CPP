87fa88d03800a68648eda58e4bcca531d4f22ebc
Deniz <s215818@dtu.dk>
2025-03-31 23:29:37 +0200

    FindtheHighestAltitude.cpp

---
+++ Problemsets/Problems/FindtheHighestAltitude.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        vector<int> v(gain.size()+1);
+        for (int i = 0; i < gain.size(); i++) {
+            v[i+1] = v[i] + gain[i];
+        }
+        return *max_element(v.begin(), v.end());
+    }
+};
