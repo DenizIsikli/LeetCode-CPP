85cc3e49b387cc81f7cc4c340a103948a931df02
Deniz <s215818@dtu.dk>
2025-04-11 22:10:47 +0200

    CountSymmetricIntegers.cpp

---
+++ Problemsets/Problems/CountSymmetricIntegers.cpp
@@ -0,0 +1,23 @@
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    int countSymmetricIntegers(int low, int high) {
+        int cnt = 0;
+        for (int i = low; i <= high; i++) {
+            string s = to_string(i);
+            int n = s.size();
+            if (n%2==0) {
+                int left = 0, right = 0;
+                for (int j = 0; j < n/2; j++) {
+                    left += s[j]-'0';
+                    right += s[n-j-1]-'0';
+                }
+                if (left==right) cnt++;
+            }
+        }
+        return cnt;
+    }
+};
