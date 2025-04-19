d01783774078c8a53d68ea7ca056cf01b5da7a46
Deniz <s215818@dtu.dk>
2025-04-20 00:22:50 +0200

    ReverseOddLevelsofBinaryTree.cpp

---
+++ Problemsets/Problems/ReverseOddLevelsofBinaryTree.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* reverseOddLevels(TreeNode* root) {
+        if (!root) return nullptr;
+        queue<TreeNode*> q;
+        vector<int> val;
+        q.push(root);
+        int level = 0;
+        while (!q.empty()) {
+            int sz = q.size();
+            vector<int> tmp;
+            for (int i = 0; i < sz; i++) {
+                TreeNode *node = q.front();
+                q.pop();
+                if (level%2) node->val = val[sz-i-1];
+                if (node->left) {
+                    q.push(node->left);
+                    tmp.push_back(node->left->val);
+                }
+                if (node->right) {
+                    q.push(node->right);
+                    tmp.push_back(node->right->val);
+                }
+            }
+            val = tmp;
+            level++;
+        }
+        return root;
+    }
+};
