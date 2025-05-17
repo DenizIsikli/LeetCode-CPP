7856657730d0598fe32d54b9e25f13d9f55093d2
Deniz Isikli <s215818@dtu.dk>
2025-05-17 22:57:36 +0200

    SortColors.cpp

--- Problemsets/Problems/SortColors.cpp
+++ Problemsets/Problems/SortColors.cpp
@@ -4,12 +4,39 @@
 class Solution {
 public:
     void sortColors(std::vector<int>& nums) {
-        for (int i = 0; i < nums.size(); ++i) {
-            for (int j = i+1; j < nums.size(); ++j) {
-                if (nums[i] > nums[j]) {
-                    std::swap(nums[i], nums[j]);
-                }
+        int zero = 0, one = 0, two = 0;
+        for (auto &x : nums) {
+            if (x==0) zero++;
+            if (x==1) one++;
+            if (x==2) two++;
+        }
+
+        for (int i = 0; i < nums.size(); i++) {
+            while (zero>0) {
+                nums[i] = 0;
+                zero--;
+                i++;
+            }
+            while (one>0) {
+                nums[i] = 1;
+                one--;
+                i++;
+            }
+            while (two>0) {
+                nums[i] = 2;
+                two--;
+                i++;
             }
         }
     }
 };
+
+// O^2 approach
+// for (int i = 0; i < nums.size(); ++i) {
+//     for (int j = i+1; j < nums.size(); ++j) {
+//         if (nums[i] > nums[j]) {
+//             std::swap(nums[i], nums[j]);
+//         }
+//     }
+// }
+
