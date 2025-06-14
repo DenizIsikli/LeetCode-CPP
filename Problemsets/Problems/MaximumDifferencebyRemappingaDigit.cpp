0258472d4af7e964d8ff4741406ab0da6fd4c611
Deniz Isikli <s215818@dtu.dk>
2025-06-14 23:30:53 +0200

    MaximumDifferencebyRemappingaDigit.cpp

--- Problemsets/Problems/MaximumDifferencebyRemappingaDigit.cpp
+++ Problemsets/Problems/MaximumDifferencebyRemappingaDigit.cpp
@@ -7,27 +7,30 @@ class Solution {
 public:
     int minMaxDifference(int num) {
         string s = to_string(num);
-        char minNum = '0', maxNum = '0';
-
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] >= '0') {
-                maxNum = s[i];
-                break;
-            }
-        }
+        char digitToMinimize = '0', digitToMaximize = '0';
 
         for (int i = 0; i < s.size(); i++) {
             if (s[i] >= '0' && s[i] < '9') {
-                minNum = s[i];
+
+                digitToMaximize = s[i];
+
                 break;
             }
         }
 
-        string  maxi = s, mini = s;
         for (int i = 0; i < s.size(); i++) {
-            if (s[i]==maxNum) maxi[i] = '0';
-            if (s[i]==minNum) mini[i] = '9';
+            if (s[i] >= '0') {
+                digitToMinimize = s[i];
+                break;
+            }
         }
-        return stoi(mini) - stoi(maxi);
+
+        string maximized = s, minimized = s;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == digitToMaximize) maximized[i] = '9';
+            if (s[i] == digitToMinimize) minimized[i] = '0';
+        }
+
+        return stoi(maximized) - stoi(minimized);
     }
 };
