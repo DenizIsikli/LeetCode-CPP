258d381cb95fe18210e3372a62cec9732e4d92e2
Deniz Isikli <s215818@dtu.dk>
2025-07-01 23:43:37 +0200

    FindGreatestCommonDivisorofArray.cpp

---
+++ Problemsets/Problems/FindGreatestCommonDivisorofArray.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int findGCD(vector<int>& nums) {
+        int mx=*max_element(nums.begin(), nums.end());
+        int mn=*min_element(nums.begin(), nums.end());
+        return __gcd(mx, mn);
+    }
+};
