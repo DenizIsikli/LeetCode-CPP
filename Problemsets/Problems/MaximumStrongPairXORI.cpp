b2c8d67708bfe36a9843371be7ef677054f80293
Deniz Isikli <s215818@dtu.dk>
2025-07-11 23:50:01 +0200

    MaximumStrongPairXORI.cpp

--- Problemsets/Problems/MaximumStrongPairXORI.cpp
+++ Problemsets/Problems/MaximumStrongPairXORI.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -6,6 +7,7 @@ class Solution {
 public:
     int maximumStrongPairXor(vector<int>& nums) {
         int ans=0;
+        sort(nums.begin(),nums.end());
         for(int i=0;i<nums.size();i++){
             for(int j=i;j<nums.size()&&nums[j]<=2*nums[i];j++){
                 ans=max(ans,nums[i]^nums[j]);
