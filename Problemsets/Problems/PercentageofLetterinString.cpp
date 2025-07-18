72482236d251e5843f9996b5cba57ef59a0b15ac
Deniz Isikli <s215818@dtu.dk>
2025-07-18 23:40:29 +0200

    PercentageofLetterinString.cpp

---
+++ Problemsets/Problems/PercentageofLetterinString.cpp
@@ -0,0 +1,14 @@
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    int percentageLetter(string s, char letter) {
+        int cnt=0;
+        for(char&c:s){
+            if(c==letter)cnt++;
+        }
+        return(cnt*100)/s.size();
+    }
+};
