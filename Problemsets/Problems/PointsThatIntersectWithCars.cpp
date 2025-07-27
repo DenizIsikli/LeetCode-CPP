670b259522b79a2c41b7f3238e8ea494d4bd140b
Deniz Isikli <s215818@dtu.dk>
2025-07-28 00:04:20 +0200

    PointsThatIntersectWithCars.cpp

--- Problemsets/Problems/PointsThatIntersectWithCars.cpp
+++ Problemsets/Problems/PointsThatIntersectWithCars.cpp
@@ -1,3 +1,4 @@
+#include <set>
 #include <vector>
 
 using namespace std;
@@ -5,19 +6,12 @@ using namespace std;
 class Solution {
 public:
     int numberOfPoints(vector<vector<int>>& nums) {
-        int n = nums.size();
-        vector<int> freq(101, 0);
-        for (int i = 0; i < n; i++) {
-            for (int j = nums[i][0]; j <= nums[i][1]; j++) {
-                freq[j]++;
-            }
+        int n=nums.size();
+        set<int>res;
+        for(int i=0;i<n;i++) {
+            int l=nums[i][0],r=nums[i][1];
+            for(int j=l;j<=r;j++)res.insert(j);
         }
-        int count = 0;
-        for (int i = 0; i < 101; i++) {
-            if (freq[i] > 0) {
-                count++;
-            }
-        }
-        return count;
+        return res.size();
     }
 };
