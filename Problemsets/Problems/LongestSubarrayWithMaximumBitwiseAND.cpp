e77ae9a16411c675338b58b7f42d1905cf587171
Deniz Isikli <s215818@dtu.dk>
2025-07-30 22:01:46 +0200

    LongestSubarrayWithMaximumBitwiseAND.cpp

--- Problemsets/Problems/LongestSubarrayWithMaximumBitwiseAND.cpp
+++ Problemsets/Problems/LongestSubarrayWithMaximumBitwiseAND.cpp
@@ -1,18 +1,20 @@
 #include <vector>
 #include <algorithm>
 
+using namespace std;
+
 class Solution {
 public:
     int longestSubarray(std::vector<int>& nums) {
-        int maxAnd=*std::max_element(nums.begin(), nums.end());
+        int maxAnd=*max_element(nums.begin(),nums.end());
         int maxLength=0,currentLength=0;
-        for (int num : nums) {
-            if (num==maxAnd)currentLength++;
+        for(int num:nums){
+            if(num==maxAnd)currentLength++;
             else{
-                maxLength=std::max(maxLength,currentLength);
+                maxLength=max(maxLength,currentLength);
                 currentLength=0;
             }
         }
-        return std::max(maxLength,currentLength);
+        return max(maxLength,currentLength);
     }
 };
