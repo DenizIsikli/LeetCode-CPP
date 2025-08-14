d810a6ec5045ed77f936fd8c3e4acb0cee8d02a3
Deniz Isikli <s215818@dtu.dk>
2025-08-14 23:26:22 +0200

    Largest3-Same-DigitNumberinString.cpp

---
+++ Problemsets/Problems/Largest3-Same-DigitNumberinString.cpp
@@ -0,0 +1,16 @@
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string largestGoodInteger(string num) {
+        string res="";
+        for(int i=0;i+2<num.size();i++){
+            if(num[i]==num[i+1]&&num[i]==num[i+2]){
+                res=max(res,num.substr(i,3));
+            }
+        }
+        return res;
+    }
+};
