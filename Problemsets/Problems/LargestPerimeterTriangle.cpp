33fc2df959249c2019271bd8a133fc25be145f50
Deniz Isikli <s215818@dtu.dk>
2025-09-28 21:12:20 +0200

    LargestPerimeterTriangle.cpp

--- Problemsets/Problems/LargestPerimeterTriangle.cpp
+++ Problemsets/Problems/LargestPerimeterTriangle.cpp
@@ -1,20 +1,17 @@
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 class Solution {
 public:
     int largestPerimeter(vector<int>& nums) {
-        int mx=0;
-        for(int i=0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                for(int k=j+1;k<nums.size();k++){
-                    if(nums[i]+nums[j]>nums[k]&&nums[j]+nums[k]>nums[i]&&nums[k]+nums[i]>nums[j]){
-                        mx=max(mx,nums[i]+nums[j]+nums[k]);
-                    }
-                }
+        sort(nums.begin(),nums.end());
+        for(int i=nums.size()-1;i>=2;i--){
+            if(nums[i-1]+nums[i-2]>nums[i]){
+                return nums[i]+nums[i-1]+nums[i-2];
             }
         }
-        return mx;
+        return 0;
     }
 };
