651ffb459adc4fb18b36d71dc5d2cbbaa257b155
Deniz Isikli <s215818@dtu.dk>
2025-10-19 23:35:22 +0200

    HexadecimalandHexatrigesimalConversion.cpp

---
+++ Problemsets/Problems/HexadecimalandHexatrigesimalConversion.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+class Solution {
+public:
+    string concatHex36(int n) {
+        long long n2=1LL*n*n;
+        long long n3=1LL*n*n*n;
+        string hex_part;
+        {
+            stringstream ss;
+            ss << uppercase << hex << n2;
+            ss >> hex_part;
+        }
+        string base36_chars="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        string base36_part;
+        while(n3>0){
+            base36_part=base36_chars[n3%36]+base36_part;
+            n3/=36;
+        }
+        return hex_part+base36_part;
+    }
+};
