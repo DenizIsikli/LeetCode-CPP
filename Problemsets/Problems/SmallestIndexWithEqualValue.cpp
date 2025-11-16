7ffe2e4e817414be6d379fe93248de143bf0bcff
Deniz Isikli <s215818@dtu.dk>
2025-11-17 00:40:04 +0100

    SmallestIndexWithEqualValue.cpp

---
+++ Problemsets/Problems/SmallestIndexWithEqualValue.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int smallestEqual(vector<int>& nums) {
+        int res=-1;
+        for(int i=0;i<nums.size();i++){
+            if(i%10==nums[i])return i;
+        }
+        return res;
+    }
+};
