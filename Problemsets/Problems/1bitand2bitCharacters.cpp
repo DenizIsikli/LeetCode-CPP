79c83546327c699f35bcb969d371a286d67231b0
Deniz Isikli <s215818@dtu.dk>
2025-11-18 23:48:23 +0100

    1bitand2bitCharacters.cpp

---
+++ Problemsets/Problems/1bitand2bitCharacters.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isOneBitCharacter(vector<int>& bits) {
+        int n=bits.size();
+        int i=0;
+        for(;i<n-1;i++){
+            if(bits[i]==1)i++;
+        }
+        return i==n-1;
+    }
+};
