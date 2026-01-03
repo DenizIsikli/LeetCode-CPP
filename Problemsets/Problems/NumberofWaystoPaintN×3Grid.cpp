e8088d7be0a3c0d41988de0210a973002eb05dcf
Deniz Isikli <s215818@dtu.dk>
2026-01-03 21:18:11 +0100

    NumberofWaystoPaintN×3Grid.cpp

---
+++ Problemsets/Problems/NumberofWaystoPaintN×3Grid.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int numOfWays(int n) {
+        const int MOD=1e9+7;
+        long long A=6,B=6;
+        for(int i=2;i<=n;i++){
+            long long newA=(A*2+B*2)%MOD;
+            long long newB=(A*2+B*3)%MOD;
+            A=newA;
+            B=newB;
+        }
+        return (A+B)%MOD;
+    }
+};
