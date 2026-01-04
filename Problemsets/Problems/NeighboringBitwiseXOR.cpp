6933368ea1d824ba281af0c1c8e2e8a068c6c019
Deniz Isikli <s215818@dtu.dk>
2026-01-04 16:03:13 +0100

    NeighboringBitwiseXOR.cpp

---
+++ Problemsets/Problems/NeighboringBitwiseXOR.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    bool doesValidArrayExist(vector<int>& derived) {
+        return accumulate(derived.begin(),derived.end(),0,bit_xor<>())==0;
+    }
+};
+
