e1fc1099161e1e5a98f1434d0794f3694e3bba8a
Deniz Isikli <s215818@dtu.dk>
2026-01-09 23:51:15 +0100

    SmallestSubtreewithalltheDeepestNodes.cpp

---
+++ Problemsets/Problems/SmallestSubtreewithalltheDeepestNodes.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int mxDepth=0;
+    TreeNode *lca=nullptr;
+    int dfs(TreeNode *root,int depth){
+        mxDepth=max(mxDepth,depth);
+        if(root==nullptr)return depth;
+        int l=dfs(root->left,depth+1);
+        int r=dfs(root->right,depth+1);
+        if(l==mxDepth&&r==mxDepth)lca=root;
+        return max(l,r);
+    }
+
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        dfs(root,0);
+        return lca;
+    }
+};
