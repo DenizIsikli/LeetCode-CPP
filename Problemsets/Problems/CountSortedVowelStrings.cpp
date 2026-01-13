f7784130de99e64815cf938a8f711335d69495ab
Deniz Isikli <s215818@dtu.dk>
2026-01-14 00:45:31 +0100

    CountSortedVowelStrings.cpp

---
+++ Problemsets/Problems/CountSortedVowelStrings.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int countVowelStrings(int n) {
+        vector<vector<long long>>dp(n+1,vector<long long>(6,0));
+        for(int j=1;j<=5;j++)dp[0][j]=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=5;j++){
+                dp[i][j]=dp[i][j-1]+dp[i-1][j];
+            }
+        }
+        return dp[n][5];
+    }
+};
