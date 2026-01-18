64f572dbe8a67db2a54bb324b0d929b7917c8d00
Deniz Isikli <s215818@dtu.dk>
2026-01-18 03:28:55 +0100

    FindtheDistanceValueBetweenTwoArrays.cpp

---
+++ Problemsets/Problems/FindtheDistanceValueBetweenTwoArrays.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
+        int res=0;
+        for(int i=0;i<arr1.size();i++){
+            int cnt=0;
+            for(int j=0;j<arr2.size();j++){
+                if(abs(arr1[i]-arr2[j])>d)cnt++;
+            }
+            if(cnt==arr2.size())res++;
+        }
+        return res;
+    }
+};
