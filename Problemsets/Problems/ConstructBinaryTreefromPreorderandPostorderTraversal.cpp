15cfd10e5cbcd309491c02c05231109a245f5bd7
Deniz Isikli <s215818@dtu.dk>
2026-02-03 23:51:33 +0100

    ConstructBinaryTreefromPreorderandPostorderTraversal.cpp

--- Problemsets/Problems/ConstructBinaryTreefromPreorderandPostorderTraversal.cpp
+++ Problemsets/Problems/ConstructBinaryTreefromPreorderandPostorderTraversal.cpp
@@ -1,16 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 class Solution {
 public:
     TreeNode* constructBST(vector<int>&preorder,vector<int>&postorder,int &idx,int start,int end){
