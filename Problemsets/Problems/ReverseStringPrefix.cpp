c8a76742910effdff7ab701f355b240086a7eff5
Deniz Isikli <s215818@dtu.dk>
2026-02-13 23:52:11 +0100

    ReverseStringPrefix.cpp

---
+++ Problemsets/Problems/ReverseStringPrefix.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    string reversePrefix(string s, int k) {
+        reverse(s.begin(),s.begin()+k);
+        return s;
+    }
+};
