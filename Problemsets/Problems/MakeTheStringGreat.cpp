f8049cd373f78f646c3167b9adb9bc6ecfd5c6e5
Deniz Isikli <s215818@dtu.dk>
2026-02-19 23:51:40 +0100

    MakeTheStringGreat.cpp

---
+++ Problemsets/Problems/MakeTheStringGreat.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    string makeGood(string s) {
+        for(int i=0;i<s.size();i++){
+            if(s[i]-s[i+1]==32||s[i]-s[i+1]==-32){
+                s.erase(i,2);
+                i=-1;
+            }
+        }
+        return s;
+    }
+};
