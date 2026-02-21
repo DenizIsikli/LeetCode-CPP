bbaa13dd3c8149f30c28c797a9ee65369d79cd54
Deniz Isikli <s215818@dtu.dk>
2026-02-21 13:11:34 +0100

    PrimeNumberofSetBitsinBinaryRepresentation.cpp

--- Problemsets/Problems/PrimeNumberofSetBitsinBinaryRepresentation.cpp
+++ Problemsets/Problems/PrimeNumberofSetBitsinBinaryRepresentation.cpp
@@ -1,30 +1,26 @@
-#include <iostream>
-
+#include<bits/stdc++.h>
 using namespace std;
-
 class Solution {
     int BrianKernighan(int n) {
-        int setBits = 0;
-        while (n) {
+        int setBits=0;
+        while(n){
             setBits+=(n&1);
             n=n>>1;
         }
         return setBits;
     }
-
     bool checkPrime(int n) {
-        if (n==1) { return 0; }
-        for (int i = 2; i <= sqrt(n); i++) {
-            if ((n%i)==0) { return 0; }
+        if(n==1){return 0;}
+        for(int i=2;i<=sqrt(n);i++){
+            if((n%i)==0){return 0;}
         }
         return 1;
     }
-
 public:
-    int countPrimeSetBits(int left, int right) {
-        int primeCount = 0;
-        for (int i = left; i <= right; i++) {
-            if (checkPrime(BrianKernighan(i))) primeCount++;
+    int countPrimeSetBits(int left,int right) {
+        int primeCount=0;
+        for (int i=left;i<=right;i++) {
+            if(checkPrime(BrianKernighan(i)))primeCount++;
         }
         return primeCount;
     }
