8df5bc7bedf13c01201b3a2d5ec791557111e4a0
Deniz Isikli <s215818@dtu.dk>
2026-02-22 21:09:58 +0100

    BinaryGap.cpp

--- Problemsets/Problems/BinaryGap.cpp
+++ Problemsets/Problems/BinaryGap.cpp
@@ -1,31 +1,17 @@
-#include <vector>
-#include <algorithm>
-
+#include<bits/stdc++.h>
 using namespace std;
-
 class Solution {
 public:
     int binaryGap(int n) {
-        vector<int> a;
-        while (n>0) {
-            int tmp = n%2;
-            a.push_back(tmp);
-            n/=2;
+        bitset<32>bits(n);
+        vector<int>ones;
+        for(int i=0;i<32;i++){
+            if(bits[i])ones.push_back(i);
         }
-
-        reverse(a.begin(), a.end());
-
-        int res = 0, size = a.size();
-        int idx = -1;
-        for (int i=0; i<size; i++) {
-            if (a[i] == 1) {
-                if (idx != -1) {
-                    res = max(res, i-idx);
-                }
-                idx = i;
-            }
+        int ans=0;
+        for(int i=1;i<ones.size();i++){
+            ans=max(ans,ones[i]-ones[i-1]);
         }
-        
-        return res;
+        return ans;
     }
 };
