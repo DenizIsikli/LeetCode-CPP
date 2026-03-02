e3612d9aa14feeeb8519dbdeb68a3200bc6e185d
Deniz Isikli <s215818@dtu.dk>
2026-03-02 02:02:23 +0100

    MinimumSwapstoArrangeaBinaryGrid.cpp

---
+++ Problemsets/Problems/MinimumSwapstoArrangeaBinaryGrid.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int minSwaps(vector<vector<int>>& grid) {
+        int n=grid.size();
+        vector<int>z(n);
+        for(int i=0;i<n;i++){
+            int cnt=0;
+            for(int j=n-1;j>=0;j--){
+                if(grid[i][j]==0)cnt++;
+                else break;
+            }
+            z[i]=cnt;
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            int need=n-i-1;
+            int j=i;
+            while(j<n&&z[j]<need)j++;
+            if(j==n)return -1;
+            while(j>i){swap(z[j],z[j-1]);j--;ans++;}
+        }
+        return ans;
+    }
+};
