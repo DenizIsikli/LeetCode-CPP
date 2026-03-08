cd73dc96916088b157bb8434f6bcebe8f1c40252
Deniz Isikli <s215818@dtu.dk>
2026-03-08 01:31:40 +0100

    FindUniqueBinaryString.cpp

--- Problemsets/Problems/FindUniqueBinaryString.cpp
+++ Problemsets/Problems/FindUniqueBinaryString.cpp
@@ -1,17 +1,12 @@
-#include <string>
-#include <vector>
-
+#include<bits/stdc++.h>
+using namespace std;
 class Solution {
 public:
     std::string findDifferentBinaryString(std::vector<std::string>& nums) {
-        std::string ans = "";
-
-        for (int i = 0; i < nums.size(); ++i) {
-            if (nums[i][i] == '1') {
-                ans += '0';
-            } else {
-                ans += '1';
-            }
+        string ans="";
+        for(int i=0;i<nums.size();++i){
+            if(nums[i][i]=='1')ans += '0';
+            else ans+='1';
         }
         return ans;
     }
