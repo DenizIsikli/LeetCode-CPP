a67ef6df6d028af266a050ca6b3c63ad6bbe8ae7
Deniz Isikli <s215818@dtu.dk>
2026-03-11 03:52:08 +0100

    ComplementofBase10Integer.cpp

---
+++ Problemsets/Problems/ComplementofBase10Integer.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#include <cstdlib>
+using namespace std;
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if(n==0)return 1;
+        int ans=0;
+        int i=0;
+        while(n>0){
+            int bit=n%2;
+            if(bit==0)ans+=(1<<i);
+            n/=2;
+            i++;
+        }
+        return ans;
+    }
+};
