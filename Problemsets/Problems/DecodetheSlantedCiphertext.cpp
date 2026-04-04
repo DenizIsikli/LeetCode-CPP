c0b6597ce87ab1727799f92ae9b4bbfa2793f2a0
Deniz Isikli <s215818@dtu.dk>
2026-04-04 02:10:15 +0200

    DecodetheSlantedCiphertext.cpp

---
+++ Problemsets/Problems/DecodetheSlantedCiphertext.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    string decodeCiphertext(string encodedText, int rows) {
+        int sz=encodedText.size();
+        vector<vector<char>>v(rows,vector<char>(sz/rows));
+        int r=0,c=0;
+        for(int i=0;i<sz;i++){
+            v[r][c]=encodedText[i];
+            c++;
+            if(c==sz/rows){
+                c=0;
+                r++;
+            }
+        }
+        string ans="";
+        for(int i=0;i<sz/rows;i++){
+            int x=0,y=i;
+            while(x<rows&&y<sz/rows){
+                ans+=v[x][y];
+                x++;
+                y++;
+            }
+        }
+        ans=ans.substr(0,ans.find_last_not_of(' ')+1);
+        return ans;
+    }
+};
