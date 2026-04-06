40b4f76bea2682f252fd00c09b6fdda724593190
Deniz Isikli <s215818@dtu.dk>
2026-04-06 02:34:49 +0200

    WalkingRobotSimulation.cpp

--- Problemsets/Problems/WalkingRobotSimulation.cpp
+++ Problemsets/Problems/WalkingRobotSimulation.cpp
@@ -1,41 +1,24 @@
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <unordered_map>
-
-class Solution {
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{
 public:
-    int robotSim(std::vector<int>& commands, std::vector<std::vector<int>>& obstacles) {
-        std::unordered_map<std::string, int> mp;
-        for (auto obst : obstacles) {
-            std::string key = std::to_string(obst[0])+"+"+std::to_string(obst[1]);
-            mp[key]++;
-        }
-
-        std::vector<std::vector<int>> direc = {{-1,0}, {0,1}, {1,0}, {0,-1}};
-
-        int dir = 1;
-        int x = 0, y = 0;
-        int res = 0;
-
-        for (int cmd : commands) {
-            if (cmd == -2) {
-                if (dir == 0) dir = 3;
-                else dir--;
-            } else if (cmd == -1) {
-                dir = (dir+1)%4;
-            } else {
-                int k = cmd;
-                for (int i = 0; i < k; i++) {
-                    int newX = x+direc[dir][0];
-                    int newY = y+direc[dir][1];
-
-                    std::string key = std::to_string(newX)+"+"+std::to_string(newY);
-                    if (mp.find(key) != mp.end()) {
-                        break;
-                    }
-                    x = newX, y = newY;
-                    res = std::max(res, (x*x)+(y*y));
+    int robotSim(vector<int>&commands,vector<vector<int>>&obstacles){
+        set<pair<int,int>>s;
+        for(auto&o:obstacles)s.insert({o[0],o[1]});
+        int x=0,y=0,dir=0,res=0;
+        for(auto&cmd:commands){
+            if(cmd==-1)dir=(dir+1)%4;
+            else if(cmd==-2)dir=(dir+3)%4;
+            else{
+                for(int k=0;k<cmd;k++){
+                    int nx=x,ny=y;
+                    if(dir==0)ny++;
+                    else if(dir==1)nx++;
+                    else if(dir==2)ny--;
+                    else nx--;
+                    if(s.count({nx,ny}))break;
+                    x=nx;y=ny;
+                    res=max(res,x*x+y*y);
                 }
             }
         }
