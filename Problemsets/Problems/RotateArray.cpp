88901131a0e83dcaa3fe982f6727aac64eafc179
Deniz Isikli <s215818@dtu.dk>
2026-04-25 20:51:02 +0200

    RotateArray.cpp

--- Problemsets/Problems/RotateArray.cpp
+++ Problemsets/Problems/RotateArray.cpp
@@ -5,11 +5,6 @@ public:
     void rotate(vector<int>& nums, int k) {
         k%=nums.size();
         reverse(nums.begin(),nums.end());
-        // cout whole vector
-        for(int i = 0; i < nums.size(); i++) {
-            cout << nums[i] << " ";
-        }
-        cout << endl;
         reverse(nums.begin(),nums.begin()+k);
         reverse(nums.begin()+k,nums.end());
     }
