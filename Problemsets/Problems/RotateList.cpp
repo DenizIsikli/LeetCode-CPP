3f2a3671d2ac30b10bcf26d66a03e2b3ed9c6de4
Deniz Isikli <s215818@dtu.dk>
2026-05-05 21:51:39 +0200

    RotateList.cpp

--- Problemsets/Problems/RotateList.cpp
+++ Problemsets/Problems/RotateList.cpp
@@ -1,32 +1,30 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+#include<bits/stdc++.h>
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        if (head == nullptr) return head;
-
-        ListNode *curr = head;
-        int n = 1;
-        while (curr->next) {
-            n++;
-            curr = curr->next;
+        vector<int>v;
+        ListNode*temp=head;
+        while(temp!=nullptr){
+            v.push_back(temp->val);
+            temp=temp->next;
         }
-        k %= n;
-        curr->next = head;
-        curr = head;
-        for (int i = 1; i < n-k; ++i) {
-            curr = curr->next;
+        int n=v.size();
+        if(n==0)return head;
+        rotate(v.begin(),v.begin()+n-k%n,v.end());
+        ListNode*ans=new ListNode(v[0]);
+        ListNode*temp1=ans;
+        for(int i=1;i<n;i++){
+            temp1->next=new ListNode(v[i]);
+            temp1=temp1->next;
         }
-        head = curr->next;
-        curr->next = nullptr;
-        return head;
+        return ans;
     }
 };
