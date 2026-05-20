21708b75cb0c84e5ad2ac8e39d254bd73974c43a
Deniz Isikli <s215818@dtu.dk>
2026-05-20 02:23:19 +0200

    FindthePrefixCommonArrayofTwoArrays.cpp

--- Problemsets/Problems/FindthePrefixCommonArrayofTwoArrays.cpp
+++ Problemsets/Problems/FindthePrefixCommonArrayofTwoArrays.cpp
@@ -1,25 +1,17 @@
-#include <vector>
-
+#include<bits/stdc++.h>
 using namespace std;
-
 class Solution {
 public:
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
-        int n = A.size();
-        vector<int> aFq(55, 0);
-        vector<int> res(n, 0);
-        int cnt = 0;
-
-        for (int i = 0; i < n; i++) {
-            aFq[A[i]]++;
-            aFq[B[i]]++;
-
-            if (aFq[A[i]] == 2) cnt++;
-            if (A[i] != B[i] && aFq[B[i]] == 2) cnt++;
-
-            res[i] = cnt;
+        int n=A.size();
+        vector<int>cnt(55,0),ans(n,0);
+        int cm=0;
+        for(int i=0;i<n;i++){
+            cnt[A[i]]++,cnt[B[i]]++;
+            if(cnt[A[i]]==2)cm++;
+            if(A[i]!=B[i]&&cnt[B[i]]==2)cm++;
+            ans[i]=cm;
         }
-
-        return res;
+        return ans;
     }
 };
