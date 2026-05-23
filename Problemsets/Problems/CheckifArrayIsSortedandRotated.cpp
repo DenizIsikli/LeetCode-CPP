8da0dce15f32ede09028f574a00f7d72e38277a2
Deniz Isikli <s215818@dtu.dk>
2026-05-23 18:54:39 +0200

    CheckifArrayIsSortedandRotated.cpp

--- Problemsets/Problems/CheckifArrayIsSortedandRotated.cpp
+++ Problemsets/Problems/CheckifArrayIsSortedandRotated.cpp
@@ -1,13 +1,11 @@
-#include <vector>
-
+#include<bits/stdc++.h>
 using namespace std;
-
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        int cnt = 0, n  = nums.size();
-        for (int i = 0; i < n; i++) {
-            if (nums[i]>nums[(i+1)%n] && ++cnt > 1) return false;
+        int cnt=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>nums[((i+1)%nums.size())]&&++cnt>1)return false;
         }
         return true;
     }
