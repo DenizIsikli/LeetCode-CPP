f9440971d8636e12dbecf0251b364756376252b0
Deniz Isikli <s215818@dtu.dk>
2026-06-09 20:00:59 +0200

    MaximumTotalSubarrayValueI.cpp

---
+++ Problemsets/Problems/MaximumTotalSubarrayValueI.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    long long maxTotalValue(vector<int>& nums, int k) {
+        long long mx=*max_element(nums.begin(),nums.end());
+        long long mn=*min_element(nums.begin(),nums.end());
+        long long ans=0;
+        for(int i=0;i<k;i++){
+            ans+=mx-mn;
+        }
+        return ans;
+    }
+};
