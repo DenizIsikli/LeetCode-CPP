9eb93f38af85269b39b74223044980581ef45106
Deniz Isikli <s215818@dtu.dk>
2026-06-14 19:29:41 +0200

    DeletetheMiddleNodeofaLinkedList.cpp

---
+++ Problemsets/Problems/DeletetheMiddleNodeofaLinkedList.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        ListNode*slow=head,*fast=head,*prev=NULL;
+        while(fast&&fast->next){
+            prev=slow;
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        if(prev)prev->next=slow->next;
+        else head=NULL;
+        return head;
+    }
+};
