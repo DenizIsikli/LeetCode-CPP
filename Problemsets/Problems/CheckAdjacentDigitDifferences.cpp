19c1db0b80a4c6987b80572923d437df3a6c0383
Deniz Isikli <s215818@dtu.dk>
2026-06-26 14:47:15 +0200

    CheckAdjacentDigitDifferences.cpp

---
+++ Problemsets/Problems/CheckAdjacentDigitDifferences.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isAdjacentDiffAtMostTwo(string s) {
+        for(int i=0;i<s.length()-1;i++){
+            if(abs(s[i]-s[i+1])>2){
+                return false;
+            }
+        }
+        return true;
+    }
+};
