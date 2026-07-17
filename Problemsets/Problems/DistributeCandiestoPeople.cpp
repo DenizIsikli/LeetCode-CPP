4da4af052e18ba439014bfd0aa8eb0cc4e41b711
Deniz Isikli <s215818@dtu.dk>
2026-07-18 00:36:26 +0200

    DistributeCandiestoPeople.cpp

--- Problemsets/Problems/DistributeCandiestoPeople.cpp
+++ Problemsets/Problems/DistributeCandiestoPeople.cpp
@@ -5,7 +5,7 @@ public:
     vector<int> distributeCandies(int candies, int num_people) {
         vector<int>res(num_people,0);
         int give=1,i=0;
-        while (candies > 0) {
+        while(candies>0) {
             res[i%num_people]+=min(give,candies);
             candies-=give;
             give++;
