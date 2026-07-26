c722ac06d76f382161a2ab3c837486ddb260958d
Deniz Isikli <s215818@dtu.dk>
2026-07-26 22:49:05 +0200

    MaximumProductofThreeNumbers.cpp

--- Problemsets/Problems/MaximumProductofThreeNumbers.cpp
+++ Problemsets/Problems/MaximumProductofThreeNumbers.cpp
@@ -1,13 +1,12 @@
-#include <vector>
-#include <algorithm>
-
+#include<bits/stdc++.h>
+using namespace std;
 class Solution {
 public:
     int maximumProduct(std::vector<int>& nums) {
-        int n = nums.size();
-        std::sort(nums.begin(), nums.end());
-        int res = 0;
-        res = std::max(nums[0]*nums[1]*nums[n-1], nums[n-1]*nums[n-2]*nums[n-3]);
+        int n=nums.size();
+        sort(nums.begin(), nums.end());
+        int res=0;
+        res=std::max(nums[0]*nums[1]*nums[n-1],nums[n-1]*nums[n-2]*nums[n-3]);
         return res;
     }
 };
