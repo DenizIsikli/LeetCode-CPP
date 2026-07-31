aeec95d69d05dd4ef16bc34173f2a720bb9d0684
Deniz Isikli <s215818@dtu.dk>
2026-07-31 19:03:07 +0200

    MinimumNumberofPushestoTypeWordII.cpp

--- Problemsets/Problems/MinimumNumberofPushestoTypeWordII.cpp
+++ Problemsets/Problems/MinimumNumberofPushestoTypeWordII.cpp
@@ -1,25 +1,17 @@
-#include <string>
-#include <vector>
-#include <algorithm>
-
+#include<bits/stdc++.h>
+using namespace std;
 class Solution {
 public:
     int minimumPushes(std::string word) {
-        int res = 0;
-        std::vector<int> freq(26, 0);
-        for (char c : word) ++freq[c-'a'];
-        std::sort(freq.begin(), freq.end(), std::greater<int>());
-
-        int keyCnt = 0;
-        int push = 1;
-
-        for (auto &v : freq) {
-            if (v == 0) break;
-            res += v*push;
-            ++keyCnt;
-            if (keyCnt % 8 == 0) ++push;
+        vector<int>v(26,0);
+        for(char&c:word)v[c-'a']++;
+        sort(v.begin(),v.end(),greater<int>());
+        int ans=0,idx=0;
+        while(idx<26&&v[idx]>0){
+            int mult=idx/8+1;
+            ans+=v[idx]*mult;
+            idx++;
         }
-
-        return res;
+        return ans;
     }
 };
