245bf2c1ef093d4410ae83b975698a1af2a6da7f
Deniz Isikli <s215818@dtu.dk>
2026-08-06 23:53:50 +0200

    SmallestDivisibleDigitProductI.cpp

--- Problemsets/Problems/SmallestDivisibleDigitProductI.cpp
+++ Problemsets/Problems/SmallestDivisibleDigitProductI.cpp
@@ -1,19 +1,17 @@
-const int INT_MAX = 2147483647;
-
+#include<bits/stdc++.h>
 using namespace std;
-
 class Solution {
 public:
     int smallestNumber(int n, int t) {
-        int res = 0;
-        for (int i = n; i < INT_MAX; i++) {
+        int res=0;
+        for(int i=n;i<INT_MAX;i++){
+            int sm=0;
             int x=1,y=i;
-            while (y) {
+            while(y){
                 x*=y%10;
                 y/=10;
-                if (x%t==0) { res=i; break; }
+                if(x%t==0){res=i;return res;}
             }
-            if (res>0) break;
         }
         return res;
     }
