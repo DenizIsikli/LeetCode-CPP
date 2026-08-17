8fd6829bafecdad6e934dff2b3becd1c10efa590
Deniz Isikli <s215818@dtu.dk>
2026-08-17 22:58:02 +0200

    LastStoneWeight.cpp

---
+++ Problemsets/Problems/LastStoneWeight.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int>pq;
+        for(auto&x:stones)pq.push(x);
+        while(pq.size()>1){
+            int y=pq.top();
+            pq.pop();
+            int x=pq.top();
+            pq.pop();
+            if(y-x!=0)pq.push(y-x);
+        }
+        if(pq.empty())return 0;
+        return pq.top();
+    }
+};
