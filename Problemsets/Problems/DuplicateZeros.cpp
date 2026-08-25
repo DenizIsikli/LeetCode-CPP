9bec42afc99c10ca542fccb3755aa9a7d290d628
Deniz Isikli <s215818@dtu.dk>
2026-08-25 23:10:47 +0200

    DuplicateZeros.cpp

---
+++ Problemsets/Problems/DuplicateZeros.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        int n=arr.size();
+        for(int i=0;i<n-1;i++){
+            if(arr[i]==0){
+                arr.insert(arr.begin()+i+1,0);
+                arr.pop_back();
+                i++;
+            }
+        }
+    }
+};
