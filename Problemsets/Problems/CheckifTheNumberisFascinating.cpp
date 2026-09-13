f1896548391eed42a53ee8d8b2abcb954ab45f6a
Deniz Isikli <s215818@dtu.dk>
2026-09-13 23:41:07 +0200

    CheckifTheNumberisFascinating.cpp

---
+++ Problemsets/Problems/CheckifTheNumberisFascinating.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isFascinating(int n) {
+        string s=to_string(n)+to_string(n*2)+to_string(n*3);
+        sort(s.begin(),s.end());
+        return s=="123456789";
+    }
+};
