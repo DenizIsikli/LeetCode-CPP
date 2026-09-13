71a38d49659e92ec2331945f2e644ba7a5121dfc
Deniz Isikli <s215818@dtu.dk>
2026-09-13 22:50:27 +0200

    CountValuesWithEquallySpacedOccurrencesI.cpp

--- Problemsets/Problems/CountValuesWithEquallySpacedOccurrencesI.cpp
+++ Problemsets/Problems/CountValuesWithEquallySpacedOccurrencesI.cpp
@@ -3,24 +3,11 @@ using namespace std;
 class Solution {
 public:
     int countSpecialIntegers(vector<int>& nums) {
-        map<int,vector<int>>mp;
-        for(int i=0;i<nums.size();i++){
-            mp[nums[i]].push_back(i);
-        }
+        unordered_map<int,vector<int>>mp;
+        for(int i=0;i<nums.size();i++)mp[nums[i]].push_back(i);
         int ans=0;
-        for(auto&[num,idx]:mp){
-            int tmp=0;
-            if(idx.size()==3){
-                int d=idx[1]-idx[0];
-                bool flag=true;
-                for(int i=2;i<idx.size();i++){
-                    if(idx[i]-idx[i-1]!=d){
-                        flag=false;
-                        break;
-                    }
-                }
-                if(flag)ans++;
-            }
+        for(auto&it:mp){
+            if(it.second.size()==3&&(abs(it.second[0]-it.second[1])==abs(it.second[1]-it.second[2])))ans++;
         }
         return ans;
     }
