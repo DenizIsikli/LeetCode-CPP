c3dc1b0badea7888abdf67b4239cd9366bb34b01
Deniz Isikli <s215818@dtu.dk>
2026-09-13 23:07:17 +0200

    CountResiduePrefixes.cpp

---
+++ Problemsets/Problems/CountResiduePrefixes.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int residuePrefixes(string s) {
+        unordered_set<int>st;
+        int ans=0;
+        for(int i=0;i<s.size();i++){
+            st.insert(s[i]);
+            if(st.size()==(i+1)%3)ans++;
+        }
+        return ans;
+    }
+};
