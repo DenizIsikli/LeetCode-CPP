e478500d85fca4487e9109f81eed6976e6d85a6c
Deniz Isikli <s215818@dtu.dk>
2026-09-19 21:33:23 +0200

    CircleandRectangleOverlapping.cpp

--- Problemsets/Problems/CircleandRectangleOverlapping.cpp
+++ Problemsets/Problems/CircleandRectangleOverlapping.cpp
@@ -1,10 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
-class Solution {
+class olution {
 public:
     bool checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
-        int x=clamp(xCenter,x1,x2)-xCenter;
-        int y=clamp(yCenter,y1,y2)-yCenter;
-        return x*x+y*y<=radius*radius;
+        int x=clamp(xCenter,x1,x2);
+        int y=clamp(yCenter,y1,y2);
+        int dx=xCenter-x;
+        int dy=yCenter-y;
+        return dx*dx+dy*dy<=radius*radius;
     }
 };
