297d0ea6561968d3df4ef93f91067eb032d16e50
Deniz Isikli <s215818@dtu.dk>
2026-09-20 23:22:09 +0200

    ReverseDegreeofaString.cpp

--- Problemsets/Problems/ReverseDegreeofaString.cpp
+++ Problemsets/Problems/ReverseDegreeofaString.cpp
@@ -1,12 +1,10 @@
-#include <string>
-
+#include<bits/stdc++.h>
 using namespace std;
-
 class Solution {
 public:
     int reverseDegree(string s) {
-        int res = 0;
-        for (int i = 0; i < s.size(); i++) {
+        int res=0;
+        for (int i=0;i<s.size();i++){
             res+=(26-(s[i]-'a'))*(i+1);
         }
         return res;
