71bf777d02d6aca2bfdbb19a0361f753025a733f
Deniz Isikli <s215818@dtu.dk>
2026-09-24 02:02:19 +0200

    SmallestIndexWithDigitSumEqualtoIndex.cpp

--- Problemsets/Problems/SmallestIndexWithDigitSumEqualtoIndex.cpp
+++ Problemsets/Problems/SmallestIndexWithDigitSumEqualtoIndex.cpp
@@ -1,18 +1,16 @@
-#include <vector>
-
+#include<bits/stdc++.h>
 using namespace std;
-
 class Solution {
 public:
     int smallestIndex(vector<int>& nums) {
-        for (int i = 0; i < nums.size(); i++) {
-            int x = nums[i];
-            int sum = 0;
-            while(x) {
-                sum += x%10;
+        for (int i=0;i<nums.size();i++){
+            int x=nums[i];
+            int sum=0;
+            while(x){
+                sum+=x%10;
                 x/=10;
             }
-            if (sum==i) {
+            if(sum==i){
                 return i;
             }
         }
